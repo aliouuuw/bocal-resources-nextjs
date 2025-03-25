d665667696e8e18e8ab727f103bb20a3f1e89c4c
aliouuuw <wadealiou00@gmail.com>
2025-03-25 22:56:04 +0000

    Update Button component to use 'asChild' prop for improved rendering in the Admin link

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen h-full space-y-4">
       <h1>Hello World</h1>
-      <Button variant="outline"><Link href="/admin">Admin</Link></Button>
+      <Button variant="outline" asChild>
+        <Link href="/admin">Admin</Link>
+      </Button>
     </div>
   );
 }
